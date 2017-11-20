1348a106088b9cc82f24be32dc13fdc454cd1dbe
Yoel-Tanujaya <31314008+Yoel-Tanujaya@users.noreply.github.com>
2017-11-20 20:07:01 +0700

    Revert "Revert "UPDATE 19/11/2017 Noon""
    This reverts commit 149c85918646c0b9411eaf8071f295bbd65b6a81.

---
+++ RPL-D7/src/netducation/PDFFileClass.java
@@ -0,0 +1,48 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package netducation;
+
+import com.lowagie.text.Document;
+import com.lowagie.text.DocumentException;
+import com.lowagie.text.PageSize;
+import com.lowagie.text.pdf.PdfContentByte;
+import com.lowagie.text.pdf.PdfTemplate;
+import com.lowagie.text.pdf.PdfWriter;
+import java.awt.Graphics2D;
+import java.io.File;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import javax.swing.JFrame;
+
+/**
+ *
+ * @author yoelt
+ */
+public class PDFFileClass {
+    public static void PrintFrameToPDF(JFrame print, String user) throws DocumentException {
+        try {
+            Document doc = new Document();
+            
+            File f = new File("C:\\Users\\yoelt\\Documents\\5th Semester\\RPL - Software Engineering\\PROJECT RPL\\PdfNetducation\\" + user + ".pdf");
+            PdfWriter writer = PdfWriter.getInstance(doc, new FileOutputStream (f));
+            doc.open();
+            PdfContentByte cb = writer.getDirectContent();
+            PdfTemplate template = cb.createTemplate(PageSize.A4.width(),PageSize.A4.height());
+            cb.addTemplate(template, 0, 0);
+            Graphics2D g2d = template.createGraphics(PageSize.A4.width(),PageSize.A4.height());
+            g2d.scale(0.98, 0.98);
+            print.print(g2d);
+            print.addNotify();
+            print.validate();
+            g2d.dispose();
+            doc.newPage();
+            doc.close();
+            
+        } catch (IOException | DocumentException e) {
+            System.out.println(e.getMessage());
+        }
+    }
+}
